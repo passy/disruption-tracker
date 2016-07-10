4b82d09f16dacb6202da8432a3e94613d0ca8586
Pascal Hartig <phartig@rdrei.net>
2016-07-10 12:29:28 +0100

    Standard parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import qualified Data.Text                as T
+import qualified Data.Text.IO             as TIO
+import qualified Options.Applicative      as Opt
+
+import           Control.Applicative      ((<**>))
+import           Data.Monoid              ((<>))
+import           Data.Version             (Version (), showVersion)
+import           Paths_disruption_tracker (version)
+import           System.Environment       (getProgName)
+
+data Options = Options
+
+options :: Opt.Parser Options
+options = pure Options
+
+cliParser :: String -> Version -> Opt.ParserInfo Options
+cliParser progName ver =
+  Opt.info ( Opt.helper <*> options <**> versionInfo )
+    ( Opt.fullDesc
+   <> Opt.progDesc "Record Tube disruptions"
+   <> Opt.header progName )
+  where
+    versionInfo = Opt.infoOption ( unwords [progName, showVersion ver] )
+      ( Opt.short 'V'
+     <> Opt.long "version"
+     <> Opt.hidden
+     <> Opt.help "Show version information" )
 
 main :: IO ()
-main = someFunc
+main = do
+  progName <- getProgName
+  Opt.execParser (cliParser progName version) >>= run
+  where
+    run :: Options -> IO ()
+    run _ = TIO.putStrLn "hello, world."
