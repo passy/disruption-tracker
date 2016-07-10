60d9b89fd043f59451edba349573509ff6c72e2b
Pascal Hartig <phartig@rdrei.net>
2016-07-10 16:49:01 +0100

    Name normalization

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE DeriveAnyClass        #-}
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Main where
 
@@ -45,8 +45,11 @@ cliParser progName ver =
 disruptionUrl :: String
 disruptionUrl = "https://citymapper.com/api/1/routestatus?weekend=0"
 
+labelModifier :: String -> String
+labelModifier = AesonC.snakeCase . drop 1
+
 aesonOptions :: Aeson.Options
-aesonOptions = Aeson.defaultOptions { Aeson.fieldLabelModifier = AesonC.snakeCase . drop 1 }
+aesonOptions = Aeson.defaultOptions { Aeson.fieldLabelModifier = labelModifier }
 
 genericParse
   :: (Generics.Generic a, Aeson.GFromJSON (Generics.Rep a))
@@ -62,57 +65,75 @@ genericToEncoding = Aeson.genericToEncoding aesonOptions
 
 data RouteDisruption = RouteDisruption
   { _disruptionSummary :: T.Text
-  , _stops   :: Maybe [T.Text]
+  , _stops             :: Maybe [T.Text]
   , _disruptionLevel   :: Int
   } deriving (Show, Eq, Generics.Generic, R.FromDatum, R.ToDatum, R.Expr)
 
+routeDisruptionNameModifier :: String -> String
+routeDisruptionNameModifier "_disruptionSummary" = "summary"
+routeDisruptionNameModifier "_disruptionLevel" = "level"
+routeDisruptionNameModifier s = labelModifier s
+
 instance Aeson.FromJSON RouteDisruption where
-  parseJSON = genericParse
+  parseJSON = Aeson.genericParseJSON $ aesonOptions { Aeson.fieldLabelModifier = routeDisruptionNameModifier }
 
 instance Aeson.ToJSON RouteDisruption where
-  toEncoding = genericToEncoding
+  toEncoding = Aeson.genericToEncoding $ aesonOptions { Aeson.fieldLabelModifier = routeDisruptionNameModifier }
 
 $(L.makeLenses ''RouteDisruption)
 
 data RouteStatus = RouteStatus
   { _statusSummary :: T.Text
-  , _description :: T.Text
-  , _statusLevel :: Int
-  , _disruptions :: [RouteDisruption]
+  , _description   :: T.Text
+  , _statusLevel   :: Int
+  , _disruptions   :: [RouteDisruption]
   } deriving (Show, Eq, Generics.Generic, R.FromDatum, R.ToDatum, R.Expr)
 
+routeStatusNameModifier :: String -> String
+routeStatusNameModifier "_statusSummary" = "summary"
+routeStatusNameModifier "_statusLevel" = "level"
+routeStatusNameModifier s = labelModifier s
+
 instance Aeson.FromJSON RouteStatus where
-  parseJSON = genericParse
+  parseJSON = Aeson.genericParseJSON $ aesonOptions { Aeson.fieldLabelModifier = routeStatusNameModifier }
 
 instance Aeson.ToJSON RouteStatus where
-  toEncoding = genericToEncoding
+  toEncoding = Aeson.genericToEncoding $ aesonOptions { Aeson.fieldLabelModifier = routeStatusNameModifier }
 
 $(L.makeLenses ''RouteStatus)
 
 data Route = Route
-  { _name   :: T.Text
-  , _status :: RouteStatus
+  { _routeName :: T.Text
+  , _status    :: RouteStatus
   } deriving (Show, Eq, Generics.Generic, R.FromDatum, R.ToDatum, R.Expr)
 
+routeNameModifier :: String -> String
+routeNameModifier "_routeName" = "name"
+routeNameModifier s = labelModifier s
+
 instance Aeson.FromJSON Route where
-  parseJSON = genericParse
+  parseJSON = Aeson.genericParseJSON $ aesonOptions { Aeson.fieldLabelModifier = routeNameModifier }
 
 instance Aeson.ToJSON Route where
-  toEncoding = genericToEncoding
+  toEncoding = Aeson.genericToEncoding $ aesonOptions { Aeson.fieldLabelModifier = routeNameModifier }
 
 $(L.makeLenses ''Route)
 
 data Grouping = Grouping
-  { _groupingName   :: T.Text
-  , _id     :: T.Text
-  , _routes :: Maybe [Route]
+  { _groupingName :: T.Text
+  , _id           :: T.Text
+  , _routes       :: Maybe [Route]
   } deriving (Show, Eq, Generics.Generic, R.FromDatum, R.ToDatum, R.Expr)
 
+groupingNameModifier :: String -> String
+groupingNameModifier "_groupingName" = "name"
+groupingNameModifier s = labelModifier s
+
 instance Aeson.FromJSON Grouping where
-  parseJSON = genericParse
+  parseJSON = Aeson.genericParseJSON $ aesonOptions { Aeson.fieldLabelModifier = groupingNameModifier }
 
 instance Aeson.ToJSON Grouping where
-  toEncoding = genericToEncoding
+  toEncoding = Aeson.genericToEncoding $ aesonOptions { Aeson.fieldLabelModifier = groupingNameModifier }
 
 $(L.makeLenses ''Grouping)
 
