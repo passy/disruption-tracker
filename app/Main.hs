aa33a9e59a3cef3799ef56cea053dcc684f1ffe4
Pascal Hartig <phartig@rdrei.net>
2016-07-10 14:57:03 +0100

    Starting a parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,19 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import qualified Data.Text                as T
 import qualified Data.Text.IO             as TIO
+import qualified Network.Wreq             as Wreq
 import qualified Options.Applicative      as Opt
+import qualified Data.Aeson.Types as Aeson
 
 import           Control.Applicative      ((<**>))
+import           Control.Lens             ((^.))
 import           Data.Monoid              ((<>))
 import           Data.Version             (Version (), showVersion)
+import           GHC.Generics             (Generic)
 import           Paths_disruption_tracker (version)
 import           System.Environment       (getProgName)
 
@@ -30,10 +35,28 @@ cliParser progName ver =
      <> Opt.hidden
      <> Opt.help "Show version information" )
 
+disruptionUrl :: String
+disruptionUrl = "https://citymapper.com/api/1/routestatus?weekend=0"
+
+data RouteStatusResponse = RouteStatusResponse
+  { last_updated_time :: T.Text
+  , groupings         :: [RouteStatus]
+  } deriving (Show, Eq, Generic)
+
+instance Aeson.FromJSON RouteStatusResponse
+
+data RouteStatus = RouteStatus
+  { name :: T.Text
+  } deriving (Show, Eq, Generic)
+
+instance Aeson.FromJSON RouteStatus
+
 main :: IO ()
 main = do
   progName <- getProgName
   Opt.execParser (cliParser progName version) >>= run
   where
     run :: Options -> IO ()
-    run _ = TIO.putStrLn "hello, world."
+    run _ = do
+      r <- Wreq.asJSON =<< Wreq.get disruptionUrl
+      print (r ^. Wreq.responseBody :: RouteStatusResponse)
