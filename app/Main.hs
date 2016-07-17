91775100a73a28596e2f6f675239941d4cb2045a
Pascal Hartig <phartig@rdrei.net>
2016-07-17 22:15:09 +0200

    Better command parsers

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,73 @@
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
 module Main where
 
-import qualified Data.Text                as T
-import qualified GHC.Generics             as Generics
+import qualified Data.Default              as Default
+import qualified Data.Text                 as T
+import qualified Data.Text.Read            as Read
 import qualified Lib
-import qualified Lib.Citymapper.Types     as C
+import qualified Lib.Citymapper.Types      as C
 import qualified Lib.DB
-import qualified Network.Wreq             as Wreq
-import qualified Options.Applicative      as Opt
-import qualified Options.Generic
+import qualified Network.Wreq              as Wreq
+import qualified Options.Applicative       as Opt
+import qualified Options.Applicative.Text  as OptT
 
-import           Control.Applicative      ((<**>))
-import           Control.Lens             (mapped, over, traverse, (^.), (^..),
-                                           _Just)
-import           Data.Monoid              ((<>))
-import           Data.Version             (Version (), showVersion)
-import           Paths_disruption_tracker (version)
-import           System.Environment       (getProgName)
+import qualified Options.Applicative.Types as Opt
+
+import           Control.Applicative       (optional, (<**>))
+import           Control.Lens              (mapped, over, traverse, (^.), (^..),
+                                            _Just)
+import           Control.Monad             (void)
+import           Data.Monoid               ((<>))
+import           Data.Version              (Version (), showVersion)
+import           Paths_disruption_tracker  (version)
+import           System.Environment        (getProgName)
 
 data Options = Options
   { optHostname :: T.Text
   , optPort     :: Integer
   , optPassword :: Maybe T.Text
-  } deriving (Generics.Generic, Show, Eq, Options.Generic.ParseRecord)
+  , optCommand  :: Command
+  } deriving (Show)
+
+instance Default.Default Options where
+  def =
+    Options "localhost" 28015 Default.def NoOp
+
+data Command = Collect
+             | Setup
+             | NoOp deriving (Show)
 
 options :: Opt.Parser Options
-options = Options.Generic.parseRecord
+options =
+  Options <$> OptT.textOption ( Opt.long "hostname"
+                             <> Opt.short 'h'
+                             <> Opt.help "RethinkDB hostname"
+                             <> Opt.value (optHostname Default.def)
+                             <> Opt.showDefault )
+          <*> integerOption ( Opt.long "port"
+                       <> Opt.short 'p'
+                       <> Opt.help "RethinkDB port"
+                       <> Opt.value (optPort Default.def)
+                       <> Opt.showDefault )
+          <*> optional ( OptT.textOption ( Opt.long "password"
+                                        <> Opt.short 'w'
+                                        <> Opt.help "RethinkDB password" ) )
+          <*> command
+
+command :: Opt.Parser Command
+command =
+  Opt.subparser (
+     Opt.command "setup" ( Opt.info (pure Setup) (Opt.progDesc "One-time database setup. Run before collect.") )
+  <> Opt.command "collect" ( Opt.info (pure Collect) (Opt.progDesc "Run a one-time collection.") ) )
+
+integer :: Opt.ReadM Integer
+integer = either error fst . Read.decimal . T.pack <$> Opt.readerAsk
+
+integerOption :: Opt.Mod Opt.OptionFields Integer -> Opt.Parser Integer
+integerOption = Opt.option integer
 
 cliParser :: String -> Version -> Opt.ParserInfo Options
 cliParser progName ver =
@@ -49,8 +87,21 @@ main = do
   progName <- getProgName
   Opt.execParser (cliParser progName version) >>= run
   where
+    host :: Options -> Lib.DB.Host
+    host opts = Lib.DB.Host (optHostname opts) (optPort opts) (optPassword opts)
+
     run :: Options -> IO ()
-    run opts = do
+    run opts = case optCommand opts of
+      Setup -> runSetup opts
+      Collect -> runCollect opts
+      NoOp -> error "Invalid command."
+
+    runSetup :: Options -> IO ()
+    runSetup opts =
+      void . Lib.DB.setup $ host opts
+
+    runCollect :: Options -> IO ()
+    runCollect opts = do
       resp <- Wreq.asJSON =<< Wreq.get (T.unpack Lib.disruptionUrl)
       let routes :: [C.Route]
           routes = resp
@@ -62,8 +113,7 @@ main = do
                  . traverse
       let extrDisruptions r = (r ^. C.routeName, r ^.. C.status . C.disruptions . traverse)
       let disruptions = over mapped extrDisruptions routes
-      let host = Lib.DB.Host (optHostname opts) (optPort opts) (optPassword opts)
-      sequence_ $ Lib.DB.writeDisruptions host . toDisruptionsRow <$> disruptions
+      sequence_ $ Lib.DB.writeDisruptions (host opts) . toDisruptionsRow <$> disruptions
 
     toDisruptionsRow :: (T.Text, [C.RouteDisruption]) -> Lib.DB.DisruptionRow
     toDisruptionsRow (name, disruptions) = Lib.DB.DisruptionRow { .. }
