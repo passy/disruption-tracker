a9f0ff30f16a70544b27b17feeca28e960b8400f
Pascal Hartig <phartig@rdrei.net>
2016-09-07 19:09:15 +0100

    Normalize the line names

--- src/Lib/DB.hs
+++ src/Lib/DB.hs
@@ -97,7 +97,8 @@ writeRoutes host routes = do
   h <- connect host
   void . R.run' h $ routesInfoTable # R.delete
   void . R.run' h $ routesInfoTable #
-    R.insert (map (\(n, url) -> ["name" R.:= n, "color" R.:= url]) routes)
+    R.insert (map (\(n, url) ->
+      ["name" R.:= T.toLower n, "display_name" R.:= n, "color" R.:= url]) routes)
 
 writeDisruptions :: Host -> LinesRow -> IO R.WriteResponse
 writeDisruptions host s = do
